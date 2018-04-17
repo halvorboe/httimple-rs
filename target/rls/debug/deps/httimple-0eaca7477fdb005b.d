226fc4b2eed5fe2298d211e0d2e5dbe11aa9d720
Mona Lisa <halvor98@outlook.com>
2018-04-17 14:54:00 +0200

    inital

---
+++ target/rls/debug/deps/httimple-0eaca7477fdb005b.d
@@ -0,0 +1,5 @@
+/Users/halvorboe/Desktop/P_NP_26_04/httimple/target/rls/debug/deps/httimple-0eaca7477fdb005b.rmeta: src/main.rs
+
+/Users/halvorboe/Desktop/P_NP_26_04/httimple/target/rls/debug/deps/httimple-0eaca7477fdb005b.d: src/main.rs
+
+src/main.rs:
---
+++ target/rls/debug/libhttimple.d
@@ -0,0 +1,1 @@
+/Users/halvorboe/Desktop/P_NP_26_04/httimple/target/rls/debug/libhttimple.rmeta: /Users/halvorboe/Desktop/P_NP_26_04/httimple/src/lib.rs /Users/halvorboe/Desktop/P_NP_26_04/httimple/src/main.rs
